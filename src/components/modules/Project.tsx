573db805d38039beaae0f257c208067c4a9f2e33
abu bakar siddik <mr.siddikdev@gmail.com>
2025-11-03 14:37:46 +0600

    update

--- src/components/modules/Project.tsx
+++ src/components/modules/Project.tsx
@@ -43,18 +43,22 @@ function Project() {
               className="w-full h-64 object-cover transition-transform duration-500 group-hover:scale-110"
             />
             {/* Overlay */}
-            <div className="absolute inset-0 bg-black/70 text-white flex flex-col justify-center items-center text-center px-4 opacity-0 group-hover:opacity-100 transition">
-              <h5 className="text-xl font-semibold mb-3 bg-gradient-to-r from-[#58bbd2] to-[#FF7E5F] bg-clip-text text-transparent">{project.title}</h5>
-              <p className="text-sm mb-4 bg-gradient-to-r  from-[#6a64dc] to-[#FF7E5F]/70 bg-clip-text text-transparent">{project.desc}</p>
-              <Link
-                to={project.link}
-                className="inline-flex items-center gap-2 px-4 py-2 bg-gradient-to-r  from-[#6a64dc] to-[#FF7E5F]/70 text-white rounded-lg hover:bg-amber-600 transition"
-              >
-                <FiExternalLink  className="bx bx-link-external text-lg"/>
-                My Projects
-              </Link>
-            </div>
+            <div className="absolute inset-0 bg-black/70 text-white flex flex-col justify-center items-center text-center px-4 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out">
+              <h5 className="text-lg sm:text-xl md:text-2xl font-semibold mb-3 bg-gradient-to-r from-[#58bbd2] to-[#FF7E5F] bg-clip-text text-transparent">
+                {project.title}
+              </h5>
+           <p className="text-xs sm:text-sm md:text-base mb-4 bg-gradient-to-r from-[#6a64dc] to-[#FF7E5F]/70 bg-clip-text text-transparent max-w-md">
+                {project.desc}
+          </p>
+            <Link
+              to={project.link}
+              className="inline-flex items-center gap-2 px-3 sm:px-4 py-2 bg-gradient-to-r from-[#6a64dc] to-[#FF7E5F]/70 text-white rounded-lg hover:scale-105 hover:shadow-lg transition-transform duration-300 ease-in-out"
+            >
+            <FiExternalLink className="text-lg" />
+          <span className="text-sm sm:text-base font-medium">My Projects</span>
+        </Link>
           </div>
+        </div>
         ))}
       </div>
     </section>
